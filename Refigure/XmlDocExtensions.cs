b3d88d08ff119ca26785bad742882bfa898dd36f
QUARK <shootdaj@gmail.com>
2016-04-15 22:12:11 -0700

    First real commit. Added base functionality

---
+++ Refigure/XmlDocExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+
+namespace Refigure
+{
+    public static class XmlDocExtensions
+    {
+        /// <summary>
+        /// Source : http://stackoverflow.com/questions/2915294/iterating-through-all-nodes-in-xml-file
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="elementVisitor"></param>
+        public static void IterateThroughAllNodes(
+            this XmlDocument doc,
+            Action<XmlNode> elementVisitor)
+        {
+            if (doc != null && elementVisitor != null)
+            {
+                foreach (XmlNode node in doc.ChildNodes)
+                {
+                    doIterateNode(node, elementVisitor);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Source : http://stackoverflow.com/questions/2915294/iterating-through-all-nodes-in-xml-file
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="elementVisitor"></param>
+        private static void doIterateNode(
+            XmlNode node,
+            Action<XmlNode> elementVisitor)
+        {
+            elementVisitor(node);
+
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                doIterateNode(childNode, elementVisitor);
+            }
+        }
+    }
+}
